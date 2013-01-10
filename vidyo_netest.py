d39f758465c9619410b9ca5c0793e8232889bdc9
unknown <bdossant@VIDYO01.cern.ch>
2013-01-10 18:01:12 +0100

    Added mail functionality

--- vidyo_netest.py
+++ vidyo_netest.py
@@ -2,6 +2,31 @@
 
 import paramiko,getpass,os,subprocess,conf
 
+messages = []
+
+def sendmail(to, message):
+  if os.name == 'nt':
+    process = subprocess.Popen("sendEmail \
+      -f avc@cern.ch \
+      -t " + to +" \
+      -u Problem(s) with Vidyo Routers Connectivity \
+      -m " + message + " \
+      -s smtp.cern.ch:587 -xu " + conf.AVC_USER + " -xp " + conf.AVC_PWD + " \
+      -o tls=yes",stdout=subprocess.PIPE)
+    process.wait()
+    response = process.returncode
+  else:
+    response = os.system("sendmail \
+      -f avc@cern.ch \
+      -t " + to +" \
+      -u Problem(s) with Vidyo Routers Connectivity \
+      -m " + message + " \
+      -s smtp.cern.ch:587 -xu " + conf.AVC_USER + " -xp " + conf.AVC_PWD + " \
+      -o tls=yes > /dev/null")
+  if response != 0:
+    print "Email was not sent!"
+  return response
+
 def ping(host):
   if os.name == 'nt':
     process = subprocess.Popen("ping -n 1 "+host,stdout=subprocess.PIPE)
@@ -10,7 +35,7 @@ def ping(host):
   else:
     response = os.system("ping -c 1 " + host +" > /dev/null")   
   if response != 0:
-    print host, 'is not reachable!'
+    messages.append(host + ' is not reachable!')
   return response
 
 def main(lr,lp):
@@ -30,13 +55,13 @@ def main(lr,lp):
       for line in lr:
         t1, t2, t3 = ssh.exec_command("nc -z " + line + " 17990; echo $?")
         if int(t2.read().strip()) != 0:  
-          print "From "+host+" port 17990 is closed in " + line
+          messages.append("From "+host+" port 17990 is closed in " + line)
       for line in lp:
         t1, t2, t3 = ssh.exec_command("nc -z " + line + " 17991; echo $?")
         if int(t2.read().strip()) != 0:
-	    print "From "+host+" port 17991 is closed in " + line
+          messages.append("From "+host+" port 17991 is closed in " + line)
     except:
-      print "SHH connection error or not open to "+host
+      messages.append("SSH connection error or not open to "+host)
 
 if __name__ == "__main__":
   lr = [line.strip() for line in (x for x in open("routers", 'r') if not x.startswith('#'))]
@@ -45,5 +70,9 @@ if __name__ == "__main__":
   lr[:] = [host for host in lr if not ping(host)]
   lp[:] = [host for host in lp if not ping(host)]
   main(lr,lp)
+  if messages:
+    str1 = "\n".join(messages)
+    print str1
+    sendmail("bruno.bompastor@cern.ch",str1)
   print "Done"
   #raw_input("Press Enter to continue...")
