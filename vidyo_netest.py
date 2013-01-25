3cac42a1600faaa051a454bec0d2292739fcb8bb
Bruno Bompastor <bruno.bompastor@cern.ch>
2013-01-25 16:00:43 +0100

    V2 (with udp checking) stable version

--- vidyo_netest.py
+++ vidyo_netest.py
@@ -1,64 +1,101 @@
 #!/usr/bin/env python
 
-import paramiko,getpass,os,subprocess,conf, inspect
+import paramiko,os,conf, inspect
 from mail_handler import cernMail
 
-messages = []
+import sys
+import socket
+import time
 
-def ping(host):
-  print "Checking Routers Connectivity..."
-  if os.name == 'nt':
-    process = subprocess.Popen("ping -n 1 "+host[0],stdout=subprocess.PIPE)
-    process.wait()
-    response = process.returncode
-  else:
-    response = os.system("ping -c 1 " + host[0] + " > /dev/null")   
-  if response != 0:
-    messages.append(host[1] + " (" + host[0] + ") is not reachable!")
+messages = []
+# Vidyo routers login credentials  
+user = conf.VIDYO_USER 
+pwd = conf.VIDYO_PWD 
+  
+sshport = 2222
+
+def ssh(host):
+  try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host[0], username=user, password=pwd, port=sshport, timeout=5)
+    ssh.close
+    response = 0
+  except:
+    print("SSH connection error or not open to " + host[1] + " (" + host[0] + ")")
+    response = 1
   return response
 
-def main(lr,lp):
-
-  # Vidyo routers login credentials  
-  user = conf.VIDYO_USER 
-  pwd = conf.VIDYO_PWD 
+def netest(host,port,type):
+  """ type => ["tcp" or "udp"] """
   
-  sshport = 2222
+  if type == "udp":
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  else:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  s.settimeout(5)
   
-  print "Testing Ports..."
-  for host in lr:
+  try:
+    if type == "udp":
+      s.sendto("0", (host[0], port))
+    else:
+      s.connect((host[0], port))
+      s.shutdown(2)
+  except Exception, e:
     try:
-      ssh = paramiko.SSHClient()
-      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      ssh.connect(host[0], username=user, password=pwd, port=sshport, timeout=5)
-      for line in lr:
-        t1, t2, t3 = ssh.exec_command("nc -z " + line[0] + " 17990; echo $?")
-        if int(t2.read().strip()) != 0:  
-          messages.append("From " + host[1] + " (" + host[0] + ") port 17990 is closed in " + line[1] + " (" + line[0] + ")")
-      for line in lp:
-        t1, t2, t3 = ssh.exec_command("nc -z " + line[0] + " 17991; echo $?")
-        if int(t2.read().strip()) != 0:
-          messages.append("From " + host[1] + " (" + host[0] + ") port 17991 is closed in " + line[1] + " (" + line[0] + ")")
-    except:
-      messages.append("SSH connection error or not open to " + host[1] + " (" + host[0] + ")")
+      errno, errtxt = e
+    except ValueError:
+      messages.append("Cannot connect to " + host[1] + " (" + host[0] + ") on TCP port: " + str(port))
+    else:
+      if errno != 107:
+        pass
+      else:
+        messages.append("Cannot connect to " + host[1] + " (" + host[0] + ") on TCP port: " + str(port))
 
+  s.close
+
+def udp(host,port):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host[0], username=user, password=pwd, port=sshport, timeout=5)
+    t1, t2, t3 = ssh.exec_command("nc -ul -w1 " + str(port))
+   
+    netest(host,port,"udp")
+ 
+    time.sleep(5)
+    ssh.close()
+    
+    if t2.read().strip() != "0":
+      messages.append("Cannot connect to " + host[1] + " (" + host[0] + ") on UDP port: " + str(port))
+    
 if __name__ == "__main__":
+
   path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
   if os.name == 'nt':
     path += "\\"
   else:
     path += "/" 
   lr = [tuple(line.strip().split(':')) for line in (x for x in open(path + "routers.txt", 'r') if not x.startswith('#'))]
-  lp = [tuple(line.strip().split(':')) for line in (x for x in open(path + "portals.txt", 'r') if not x.startswith('#'))]
-  #lr[:] = [host for host in lr if not ping(host)]
-  #lp[:] = [host for host in lp if not ping(host)]
-  main(lr,lp)
+  
+  print "Testing TCP Ports On Routers..."
+  for host in lr:
+    netest(host,443,"tcp")
+    netest(host,17990,"tcp")
+  
+  print "Checking Routers SSH Connectivity..."
+  lr[:] = [host for host in lr if not ssh(host)]
+  
+  print "Testing UDP Ports On Routers..."
+  for host in lr:
+    udp(host,50500)
+
   if messages:
     body = "\n".join(messages)
-    fr0m = "service-avc-operation@cern.ch"
-    to = ["service-avc-operation@cern.ch"]
-    sub = "[Vidyo] Problem(s) with Vidyo Routers Connectivity"
-    m = cernMail(fr0m, to, sub, body)
-    m.send()
+    print body
+    #fr0m = "service-avc-operation@cern.ch"
+    #to = ["service-avc-operation@cern.ch"]
+    #sub = "[Vidyo] Problem(s) with Vidyo Routers Connectivity"
+    #m = cernMail(fr0m, to, sub, body)
+    #m.send()
     
   print "Done"
