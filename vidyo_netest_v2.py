70af545081a002327705925126a1ef843aa074e2
Bruno Bompastor <bruno.bompastor@cern.ch>
2013-01-25 10:37:35 +0100

    Stable version with udp and tcp testing

--- vidyo_netest_v2.py
+++ vidyo_netest_v2.py
@@ -27,44 +27,47 @@ def ssh(host):
   return response
 
 def netest(host,port,type):
-  # type => ["tcp" or "udp"]
+  """ type => ["tcp" or "udp"] """
+  
   if type == "udp":
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   else:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.settimeout(5)
-  try:
-    if type == "udp":
+  
+  if type == "udp":
+    try:
       s.sendto("0", (host[0], port))
-      recv, svr = s.recvfrom(255)
-      s.shutdown(2)
-    else:
+    except Exception:
+      pass
+  else:
+    try:
       s.connect((host[0], port))
       s.shutdown(2)
-  except Exception, e:
-    try:
-      errno, errtxt = e
-    except ValueError:
-      messages.append("Cannot connect to " + host[0] + " on port: " + str(port))
-    else:
-      if errno != 107:
-        messages.append("Cannot connect to " + host[0] + " on port: " + str(port))
-
+    except Exception, e:
+      try:
+        errno, errtxt = e
+      except ValueError:
+        messages.append("Cannot connect to " + host[0] + " on TCP port: " + str(port))
+      else:
+        if errno != 107:
+          messages.append("Cannot connect to " + host[0] + " on TCP port: " + str(port))
+  
   s.close
 
 def udp(host,port):
-    ssh1 = paramiko.SSHClient()
-    ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh1.connect(host[0], username=user, password=pwd, port=sshport, timeout=5)
-    t1, t2, t3 = ssh1.exec_command("nc -ul -w1 " + str(port))
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host[0], username=user, password=pwd, port=sshport, timeout=5)
+    t1, t2, t3 = ssh.exec_command("nc -ul -w1 " + str(port))
    
     netest(host[0],port,"udp")
  
     time.sleep(1)
-    ssh1.close()
+    ssh.close()
     
     if t2.read().strip() != "0":
-      messages.append("Cannot connect to " + host[0] + " on port: " + str(port))
+      messages.append("Cannot connect to " + host[0] + " on UDP port: " + str(port))
     
 if __name__ == "__main__":
 
@@ -75,16 +78,16 @@ if __name__ == "__main__":
     path += "/" 
   lr = [tuple(line.strip().split(':')) for line in (x for x in open(path + "routers.txt", 'r') if not x.startswith('#'))]
   
-  print "Testing TCP Ports From Routers..."
+  print "Testing TCP Ports On Routers..."
   for host in lr:
+    netest(host,443,"tcp")
     netest(host,17990,"tcp")
-    netest(host,17991,"tcp")
     netest(host,17992,"tcp")
   
   print "Checking Routers SSH Connectivity..."
   lr[:] = [host for host in lr if not ssh(host)]
   
-  print "Testing UDP Ports From Routers..."
+  print "Testing UDP Ports On Routers..."
   for host in lr:
     udp(host,50000)
 
